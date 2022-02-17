b8a739ffa6d6688b4dcff4544ab423a4f0f65555
oliviaamiller <oliviaoliviaolivia@gmail.com>
2022-02-17 11:48:31 -0800

    button click test working!

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,20 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/flamingo/i);
   expect(linkElement).toBeInTheDocument();
+});
+
+test('isOpen = true when you click the good morning! button', () => {
+  render(<App />);
+  const openButtonElement = screen.getByRole('button', { name: /Good Morning!/i });
+
+  fireEvent.click(openButtonElement);
+
+  const openSignElement = screen.getByText(/open/i);
+
+
+  expect(openSignElement).toBeInTheDocument();
 });
